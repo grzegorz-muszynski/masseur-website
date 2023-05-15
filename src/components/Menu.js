07e5b50fabf1956d8631ae431ba242bb4d6f3982
GregV3 <grzegorz.muszynski77@gmail.com>
2023-05-15 12:31:36 +0200

    table of services completed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,7 +12,7 @@ export default function Menu() {
                 <li><NavLink to='/prices'>Cennik</NavLink></li>
                 <li><NavLink to='/vouchery'>Vouchery</NavLink></li>
                 <li><NavLink to='/forpatient'>Dla pacjenta</NavLink></li>
-                <li><NavLink to='/about'>O nas</NavLink></li>
+                <li><NavLink to='/about'>Nasz zespół</NavLink></li>
                 <li><NavLink to='/opinions'>Opinie</NavLink></li>
                 <li><NavLink to='/localisation'>Lokalizacja</NavLink></li>
                 <li><NavLink to='/contact'>Kontakt</NavLink></li>
--- src/pages/Prices.js
+++ src/pages/Prices.js
@@ -5,7 +5,155 @@ export default function Prices() {
 
     return (
         <div className='Prices'>
-            foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+            <table>
+                <tr>
+                    <th>Rodzaj masażu</th>
+                    <th>Czas</th>
+                    <th>Cena</th>
+                </tr>
+                <tr>
+                    <td colspan="3">Masaże przeciwbólowe, medyczne</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Aneta</td>
+                    <td>30 min.</td>
+                    <td>70 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Aneta</td>
+                    <td>45 min.</td>
+                    <td>100 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Aneta</td>
+                    <td>60 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Dmytro</td>
+                    <td>30 min.</td>
+                    <td>70 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Dmytro</td>
+                    <td>45 min.</td>
+                    <td>100 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż klasyczny przeciwbólowy Dmytro</td>
+                    <td>60 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż tkanek głębokich (MTG) Aneta</td>
+                    <td>30 min.</td>
+                    <td>70 zł</td>
+                </tr>
+                <tr>
+                    <td>Manualny drenaż limfatycznny (MDL) Aneta</td>
+                    <td>45 min.</td>
+                    <td>100 zł</td>
+                </tr>
+                <tr>
+                    <td>Manualny drenaż limfatycznny (MDL) Aneta</td>
+                    <td>60 min.</td>
+                    <td>120 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż leczniczy Aneta</td>
+                    <td>30 min.</td>
+                    <td>70 zł</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Masaże profilaktyczne</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Aneta</td>
+                    <td>30 min.</td>
+                    <td>100 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Aneta</td>
+                    <td>45 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Aneta</td>
+                    <td>60 min.</td>
+                    <td>150 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Dmytro</td>
+                    <td>30 min.</td>
+                    <td>100 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Dmytro</td>
+                    <td>45 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż profilaktyczny Dmytro</td>
+                    <td>60 min.</td>
+                    <td>150 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż gorącymi kamieniamni Aneta</td>
+                    <td>60 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż balijski Aneta</td>
+                    <td>60 min.</td>
+                    <td>130 zł</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Masaże dla sportowców</td>
+                </tr>
+                <tr>
+                    <td>Masaż sportowy Aneta</td>
+                    <td>30 min.</td>
+                    <td>70 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż kreolski (bambusami) Aneta</td>
+                    <td>30 min.</td>
+                    <td>60 zł</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Masaże odprężające, pielęgnacyjne</td>
+                </tr>
+                <tr>
+                    <td>Masaż bańką chińską Aneta</td>
+                    <td>30 min.</td>
+                    <td>65 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż bańką chińską Aneta</td>
+                    <td>45 min.</td>
+                    <td>80 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż bańką chińską Dmytro</td>
+                    <td>30 min.</td>
+                    <td>65 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż Kobido face Aneta</td>
+                    <td>90 min.</td>
+                    <td>160 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż gorącą świecą Aneta</td>
+                    <td>60 min.</td>
+                    <td>140 zł</td>
+                </tr>
+                <tr>
+                    <td>Masaż gorącą świecą Dmytro</td>
+                    <td>60 min.</td>
+                    <td>125 zł</td>
+                </tr>
+            </table>
         </div>
     )
 }
