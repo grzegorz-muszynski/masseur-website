c7f6c30382ff698bd42d751d34a766861d36a107
GregV3 <grzegorz.muszynski77@gmail.com>
2024-02-21 19:22:59 +0100

    Removing one location

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -9,9 +9,6 @@ export default function Start() {
             <h2>zaprasza na zabiegi do salonu</h2>
             <h2><span>ODNOWA</span></h2>
             <h2>przy ul. Mickiewicza 3 w Miechowie</h2>
-            <h2>oraz do gabinetów</h2>
-            <h2><span>Angel Nails</span></h2>
-            <h2>przy ul. Warszawskiej 168 w Bibicach</h2>
         
             <p className="Start__text">Pracujemy 6 dni w tygodniu. Aktualny grafik wraz z możliwością <a href="https://booksy.com/pl-pl/86946_odnowa_masaz_10052_miechow">zapisów on-line</a> dostępny jest na naszej <a href="https://booksy.com/pl-pl/86946_odnowa_masaz_10052_miechow">wizytówce</a> w portalu Booksy.</p>
         
