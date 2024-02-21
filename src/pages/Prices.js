4121a302c052a912b79ab448f75194e6653a2c50
GregV3 <grzegorz.muszynski77@gmail.com>
2024-02-21 19:51:12 +0100

    Prices changed

--- src/pages/Prices.js
+++ src/pages/Prices.js
@@ -4,127 +4,30 @@ export default function Prices() {
     
     return (
         <div className='Prices'>
+            <h3>Nasz cennik</h3>
             <table>
                 <tbody>
                     <tr>
-                        <td colSpan="3">Masaże przeciwbólowe, medyczne</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż klasyczny przeciwbólowy</td>
                         <td>30 min.</td>
-                        <td>70 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż klasyczny przeciwbólowy</td>
-                        <td>45 min.</td>
-                        <td>100 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż klasyczny przeciwbólowy</td>
-                        <td>60 min.</td>
-                        <td>130 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż tkanek głębokich (MTG)</td>
-                        <td>30 min.</td>
-                        <td>70 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Manualny drenaż limfatycznny (MDL)</td>
-                        <td>45 min.</td>
-                        <td>100 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Manualny drenaż limfatycznny (MDL)</td>
-                        <td>60 min.</td>
-                        <td>120 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż leczniczy</td>
-                        <td>30 min.</td>
-                        <td>70 zł</td>
-                    </tr>
-                </tbody>   
-            </table>
-
-            <table>
-                <tbody>
-                    <tr>
-                        <td colSpan="3">Masaże profilaktyczne</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż profilaktyczny</td>
-                        <td>30 min.</td>
-                        <td>100 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż profilaktyczny</td>
-                        <td>45 min.</td>
-                        <td>130 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż profilaktyczny</td>
-                        <td>60 min.</td>
-                        <td>150 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż gorącymi kamieniamni</td>
-                        <td>60 min.</td>
-                        <td>130 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż balijski</td>
-                        <td>60 min.</td>
-                        <td>130 zł</td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <table>
-                <tbody>
-                    <tr>
-                        <td colSpan="3">Masaże dla sportowców</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż sportowy</td>
-                        <td>30 min.</td>
-                        <td>70 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż kreolski (bambusami)</td>
-                        <td>30 min.</td>
-                        <td>60 zł</td>
-                    </tr>
-                </tbody>
-            </table>
-
-
-            <table>
-                <tbody>
-                    <tr>
-                        <td colSpan="3">Masaże odprężające, pielęgnacyjne</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż bańką chińską</td>
-                        <td>30 min.</td>
-                        <td>65 zł</td>
-                    </tr>
-                    <tr>
-                        <td>Masaż bańką chińską</td>
-                        <td>45 min.</td>
                         <td>80 zł</td>
                     </tr>
                     <tr>
-                        <td>Masaż Kobido face</td>
-                        <td>90 min.</td>
-                        <td>160 zł</td>
+                        <td>45 min.</td>
+                        <td>110 zł</td>
                     </tr>
                     <tr>
-                        <td>Masaż gorącą świecą</td>
                         <td>60 min.</td>
                         <td>140 zł</td>
                     </tr>
-                </tbody>
+                    <tr>
+                        <td>75 min.</td>
+                        <td>160 zł</td>
+                    </tr>
+                    <tr>
+                        <td>90 min.</td>
+                        <td>180 zł</td>
+                    </tr>
+                </tbody>   
             </table>
         </div>
     )
