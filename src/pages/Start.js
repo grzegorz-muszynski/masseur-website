81da19665efca76707865e361fa954b67a508f7f
grzegorz-muszynski <grzegorz.muszynski77@gmail.com>
2025-02-15 13:52:13 +0100

    OnlineBookingBtn component done

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Start.css";
+import OnlineBookingBtn from "../components/OnlineBookingBtn";
 
 export default function Start() {
 
@@ -10,11 +11,9 @@ export default function Start() {
             <h2><span>ODNOWA</span></h2>
             <h2>przy ul. Mickiewicza 3 w Miechowie</h2>
         
-            <p className="Start__text">Pracujemy 6 dni w tygodniu. Aktualny grafik wraz z możliwością <a href="https://booksy.com/pl-pl/86946_odnowa_masaz_10052_miechow">zapisów on-line</a> dostępny jest na naszej <a href="https://booksy.com/pl-pl/86946_odnowa_masaz_10052_miechow">wizytówce</a> w portalu Booksy.</p>
+            <OnlineBookingBtn />
         
-            <h2><span>Rejestracja</span></h2>
-            <p>+48 575 857 130</p>
-            <p>anetakubik84@gmail.com</p>
+            <h2><span>Rejestracja tel./SMS: 57 58 57 130</span></h2>
         </div>
     )
 }
