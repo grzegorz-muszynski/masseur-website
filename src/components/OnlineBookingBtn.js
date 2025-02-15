0909c9d35f2cc623a61653cd4c9b668fdf22c70a
grzegorz-muszynski <grzegorz.muszynski77@gmail.com>
2025-02-15 14:46:48 +0100

    online btn done responsive

--- src/components/OnlineBookingBtn.js
+++ src/components/OnlineBookingBtn.js
@@ -2,10 +2,9 @@ import React from 'react';
 import './OnlineBookingBtn.css';
 
 export default function OnlineBookingBtn({ hasBackground }) {
-
-    console.log(hasBackground)
     const buttonClass = hasBackground ? "onlineBtn pulse" : "onlineBtn";
 
+    console.log(hasBackground)
     return (
         <a href="https://booksy.com/pl-pl/86946_odnowa_masaz_10052_miechow" className={buttonClass}>
             UMÓW WIZYTĘ ON-LINE
