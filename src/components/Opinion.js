c6ee04f57f063c96aad1cfa61073690375ae8dd1
grzegorz-muszynski <grzegorz.muszynski77@gmail.com>
2025-02-16 18:10:38 +0100

    bad opinions removed

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -6,7 +6,7 @@ export default function Opinion ({ first_name, last_name, rank, review }) {
         <div className="container">
             {rank === 5 && <div className="rank">{"*****"}</div>}
             <div className="review">{review}</div>
-            <div className="author">{`${first_name} ${last_name}`}</div>
+            <div className="author">{`${first_name} ${last_name[0]}.`}</div>
         </div>
     );
 }
