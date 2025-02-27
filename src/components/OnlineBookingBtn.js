0378bea429a565e5d1e4076f217e58bf4ebb0a02
grzegorz-muszynski <grzegorz.muszynski77@gmail.com>
2025-02-27 18:08:31 +0100

    text prop for OnlineBookingBtn implemented

--- src/components/OnlineBookingBtn.js
+++ src/components/OnlineBookingBtn.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './OnlineBookingBtn.css';
 
-export default function OnlineBookingBtn({ hasBackground }) {
+export default function OnlineBookingBtn({ hasBackground, text='UMÓW WIZYTĘ ON-LINE' }) {
     const buttonClass = hasBackground ? "onlineBtn pulse" : "onlineBtn";
 
     return (
         <a href="https://appointment-booking-six.vercel.app/" className={buttonClass}>
-            UMÓW WIZYTĘ ON-LINE
+            {text}
         </a>
     );
 }
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -6,12 +6,10 @@ export default function Start() {
 
     return (
         <div className="Start">
-            <h2><span>Aneta Kubik</span></h2>
-            <h2>zaprasza na zabiegi do salonu</h2>
-            <h2><span>ODNOWA</span></h2>
-            <h2>przy ul. Mickiewicza 3 w Miechowie</h2>
+            <h2>Zapraszamy na zabiegi masażu do Salonu <span>Odnowa</span> oraz zajęcia sportowo-rekreacyjne do sali ćwiczeń.</h2>
+            <h2><span>Miechów, ul. A. Mickiewicza 3</span></h2>
             <h2><span>Rejestracja tel./SMS: 57 58 57 130</span></h2>
-            <OnlineBookingBtn hasBackground={true} />
+            <OnlineBookingBtn hasBackground={true} text={'Rezerwuj stół do ping ponga'} />
         </div>
     )
 }
